8a9f115f0b0649e09b0df73bfea382baeaed1907
王海秋 <1804006798@qq.com>
2025-05-09 10:27:09 +0800

    基本的引用计数系统来终止glfw

--- Wizar/src/Platform/Windows/WindowsWindow.cpp
+++ Wizar/src/Platform/Windows/WindowsWindow.cpp
@@ -10,7 +10,7 @@
 
 namespace Wizar {
 	
-	static bool s_GLFWInitialized = nullptr;
+	static uint8_t s_GLFWWindowCount = 0;
 
 	static void GLFWErrorCallback(int error, const char* description) 
 	{
@@ -25,6 +25,12 @@ namespace Wizar {
 	void WindowsWindow::Shutdown()
 	{
 		glfwDestroyWindow(m_Window);
+
+		if (--s_GLFWWindowCount == 0)
+		{
+			WZ_CORE_INFO("Terminating GLFW");
+			glfwTerminate();
+		}
 	}
 
 
@@ -45,17 +51,19 @@ namespace Wizar {
 
 		WZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
-		if (!s_GLFWInitialized)
+		if (s_GLFWWindowCount == 0)
 		{
 			//TODO: glfwTerminate on system shutdown
+			WZ_CORE_INFO("Initializing GLFW");
 			int success = glfwInit();
 			WZ_CORE_ASSERT(success, "Could not intialize GLFW!");
 			glfwSetErrorCallback(GLFWErrorCallback);
-			s_GLFWInitialized = true;
+		
 		}
 
 		//´´½¨GLFW´°¿Ú
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		++s_GLFWWindowCount;
 
 		m_Context = CreateScope<OpenGLContext>(m_Window);
 		m_Context->Init();
